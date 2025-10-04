61f7565b973f2b1f2e022ad876932235165d7e7d
Ephemeral <theephemeral.txt@gmail.com>
2025-10-04 15:26:45 +0600

    feat: rename fork...

--- fang_test.go
+++ fang_test.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"testing"
 
-	"github.com/charmbracelet/fang"
+	"github.com/Nadim147c/fang"
 	"github.com/charmbracelet/x/exp/golden"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
